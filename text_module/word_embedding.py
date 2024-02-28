f04b9448dcb8d5b5935941abd07e51e2d9007412
DanQuang <21521917@gm.uit.edu.vn>
2024-02-28 23:54:48 +0700

    update

--- text_module/word_embedding.py
+++ text_module/word_embedding.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from data_utils.vocab import Vocab
-from data_utils.utils import padding_sequence
 
 class WordEmbedding(nn.Module):
     def __init__(self, config):
@@ -14,12 +13,6 @@ class WordEmbedding(nn.Module):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         
     def forward(self, input_texts):
-        sequence_vectors = []
-        for input_text in input_texts:
-            padding_seq = padding_sequence(input_text, self.max_length, self.vocab.pad_token_idx())
-            sequence_vectors.append(padding_seq)
-        
-        padding_sequences = torch.stack(sequence_vectors, dim= 0).to(self.device)
-        output = self.embedding(padding_sequences)
+        output = self.embedding(input_texts)
         output = self.droput(output)
         return output
